c55a0d5d90702c47809a73313a02618fc318a3e2
Jay T <jaytula@gmail.com>
2020-06-17 14:14:12 -0700

    TypeScript Basics: Working with Tuples

--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -1,9 +1,20 @@
 // const person: { name: string; age: number } = {
-const person = {
+const person: {
+  name: string,
+  age: number;
+  hobbies: string[],
+  role: [number, string];
+} = {
   name: "Maximilian",
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
+
+// person.role.push('admin'); // exception... allowed
+// person.role[1] = 10;  // ERR
+// person.role []; // ERR
+// person.role = [0, 'admin', 'user']; // ERR
 
 let favoriteActivites: string[];
 favoriteActivites = ['Sports'];
