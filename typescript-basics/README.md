b9c7731876ef6ecd823065fdbae488470d14f25b
Jay T <jaytula@gmail.com>
2020-06-20 16:45:24 -0700

    TypeScript Compiler: Using "Watch Mode"

---
+++ typescript-basics/README.md
@@ -0,0 +1,7 @@
+## Notes
+
+### Watch Mode
+
+```shell
+tsc --watch app.ts
+```
--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -1,16 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = "Max";
 
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message, errorCode: code };
-  // while(true) {}
-}
-
-const result = generateError("An error occurred!", 500);
-console.log(result);
+console.log(userName);
