1b345c24043771e74be9db3dfeea9f55ca9d880f
Jay T <jaytula@gmail.com>
2020-06-20 12:26:44 -0700

    TypeScript Basics: The "unknown" Type

---
+++ typescript-basics/functions.ts
@@ -0,0 +1,17 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = 5;
+// combineValues = printResult;
+
+console.log(combineValues(8, 9));
