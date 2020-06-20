ad725318ac4757660285994ba3366ff1856bfc97
Jay T <jaytula@gmail.com>
2020-06-20 13:21:38 -0700

    TypeScript Basics: The "never" Type

--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -6,3 +6,11 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+  // while(true) {}
+}
+
+const result = generateError("An error occurred!", 500);
+console.log(result);
