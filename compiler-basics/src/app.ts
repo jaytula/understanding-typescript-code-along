3a9ee381522b0b151ceb80245860fc586f838d52
Jay T <jaytula@gmail.com>
2020-06-23 14:00:49 -0700

    TypeScript Compiler: Code Quality Options

--- compiler-basics/src/app.ts
+++ compiler-basics/src/app.ts
@@ -1,10 +1,24 @@
 const button = document.querySelector("button");
 
-function clickHandler(message: string) {
-  console.log('Clicked! ' + message);
+let appId = "abc";
+
+// No implicit returns example
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+
+  // return;
+}
+
+function clickHandler(message: string, age: number) {
+  let userName = "Max";
+  console.log("Clicked! " + message);
 }
 
 if (button) {
-  button.addEventListener("click", clickHandler.bind(null, "You're welcome"));
+  button.addEventListener(
+    "click",
+    clickHandler.bind(null, "You're welcome", 32)
+  );
 }
-
