0ca56aab970ea11da18c3a3074c0e645632d495d
Jay T <jaytula@gmail.com>
2020-06-24 14:18:56 -0700

    Next-gen: Arrow Functions

--- next-gen/src/app.ts
+++ next-gen/src/app.ts
@@ -4,12 +4,12 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
+// function add(a: number, b: number) {
+//   let result;
 
-  result = a + b;
-  return result;
-}
+//   result = a + b;
+//   return result;
+// }
 
 // var has function and global scope
 // let also has block scope
@@ -20,3 +20,14 @@ function add(a: number, b: number) {
 // console.log(isOld);
 
 // console.log(result);
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) [button.addEventListener("click", event => console.log(event))];
+printOutput(add(3, 8));
