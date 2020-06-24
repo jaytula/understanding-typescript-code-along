e1a7a8bbc9af0bac17b99bdcd900ca7e432a8ab9
Jay T <jaytula@gmail.com>
2020-06-24 16:10:53 -0700

    Next-gen: Default Function Parameters

--- next-gen/src/app.ts
+++ next-gen/src/app.ts
@@ -21,7 +21,7 @@ age = 29;
 
 // console.log(result);
 
-const add = (a: number, b: number) => a + b;
+const add = (a: number, b: number = 1) => a + b;
 
 console.log(add(2, 5));
 
@@ -30,4 +30,4 @@ const printOutput: (a: number | string) => void = output => console.log(output);
 const button = document.querySelector("button");
 
 if (button) [button.addEventListener("click", event => console.log(event))];
-printOutput(add(3, 8));
+printOutput(add(3));
