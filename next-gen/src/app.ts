5b2d201bbdd7bcba2c635a46e2c229b2502ffddf
Jay T <jaytula@gmail.com>
2020-06-24 00:24:02 -0700

    Next-gen: "let" and "const"

--- next-gen/src/app.ts
+++ next-gen/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+const userName = "Max";
+// userName = 'Maximilian';
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+
+  result = a + b;
+  return result;
+}
+
+// var has function and global scope
+// let also has block scope
+// if (age > 0) {
+//   var isOld = true;
+// }
+
+// console.log(isOld);
+
+// console.log(result);
