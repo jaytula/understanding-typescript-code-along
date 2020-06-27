f3e7c166b8dd18541d11279697d5224a32220da0
Jay T <jaytula@gmail.com>
2020-06-27 12:41:10 -0700

    Interfaces: Getters & Setters

--- ts-interfaces/src/app.ts
+++ ts-interfaces/src/app.ts
@@ -31,16 +31,35 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+
+    throw new Error('No report found');
+  }
+
+  set mostRecentReport(value: string) {
+    if(!value) {
+      throw new Error('Please pass in a valid value!');
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   addEmployee(text: string) {
-    if (text === 'Max') {
+    if (text === "Max") {
       return;
     }
 
@@ -64,12 +83,16 @@ it.printEmployeeInformation();
 console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
-accounting.addReport('Something went wrong');
-accounting.printReports();
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-accounting.printEmployeeInformation();
 
+accounting.addReport("Something went wrong");
+
+accounting.mostRecentReport = 'abcd';
+console.log(accounting.mostRecentReport);
+
+accounting.printReports();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.printEmployeeInformation();
 
 // const itCopy = { name: 'DUMMY', describe: it.describe };
 
