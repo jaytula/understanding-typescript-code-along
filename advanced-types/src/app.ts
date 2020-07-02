bcc08cf61f754c86136b2b534786d95df911dda6
Jay T <jaytula@gmail.com>
2020-07-01 23:43:51 -0700

    Advanced Types: Type Casting

--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -103,3 +103,13 @@ function moveAnimal(animal: Animal) {
 
 moveAnimal({ type: "bird", flyingSpeed: 10 });
 moveAnimal({ type: "horse", runningSpeed: 15 });
+
+const paragraph = document.getElementById("message-output");
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+// const userInputElement = document.getElementById('user-input') as HTMLInputElement;
+const userInputElement = document.getElementById("user-input");
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there";
+}
