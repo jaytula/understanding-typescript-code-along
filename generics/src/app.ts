deeca7a21ac4c4a2de1a3b7262543966a9fde0b5
Jay T <jaytula@gmail.com>
2020-07-04 12:53:00 -0700

    Generics: Creating a Generic Function

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -10,3 +10,10 @@
 // promise.then(data => {
 //   // data.split(' ');
 // })
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj);
