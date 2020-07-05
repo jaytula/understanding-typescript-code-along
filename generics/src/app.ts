8e9b4f9d4e713cf75d7fd060a548a1bfe0ff11de
Jay T <jaytula@gmail.com>
2020-07-04 18:22:49 -0700

    Generics: Another Generic Function

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -17,3 +17,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length > 0) {
+    descriptionText = 'Got ' + element.length + ' element(s).';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there'));
+console.log(countAndDescribe(''));
+console.log(countAndDescribe(['asdfasd']));
+console.log(countAndDescribe([]));
