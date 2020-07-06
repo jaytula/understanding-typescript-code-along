3ae370485a703e258905d6fe8483404869f4954e
Jay T <jaytula@gmail.com>
2020-07-06 11:27:30 -0700

    Decorators: Working with Decorator Factories

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -1,14 +1,16 @@
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+@Logger('LOGGING - PERSON')
 class Person {
-  name = 'Max';
+  name = "Max";
 
   constructor() {
-    console.log('Creating person object...');
+    console.log("Creating person object...");
   }
 }
 
