af2025770645c623bfdc508d4ef42dc70160714f
Jay T <jaytula@gmail.com>
2020-07-08 01:33:22 -0700

    Decorators: Example: Creating an "Autobind" Decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -47,6 +47,7 @@ function Log(target: any, propertyName: string | Symbol) {
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
   console.log('Accessor decorator');
   console.log({target, name, descriptor});
+  return {}
 }
 
 function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
@@ -88,3 +89,31 @@ class Product {
 
 const p1 = new Product('Book 1', 19);
 const p2 = new Product('Book 2', 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescriptor;
+}
+
+class Printer {
+  message = 'This works!';
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', p.showMessage);
