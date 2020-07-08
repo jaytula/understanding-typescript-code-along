c0c3427982556cee30e9b54bad0fd12c9d6d4a03
Jay T <jaytula@gmail.com>
2020-07-07 17:55:34 -0700

    Decorators: Returning (and changing) a Class in a Class Decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -8,13 +8,17 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log("TEMPLATE FACTORY");
-  return function (constructor: any) {
-    console.log("Rendering Template");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
+  return function<T extends {new(...args: any[]): {name: string}}> (originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering Template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
     }
   };
 }
@@ -30,8 +34,8 @@ class Person {
 }
 
 const pers = new Person();
-
 console.log(pers);
+
 
 // ---
 function Log(target: any, propertyName: string | Symbol) {
