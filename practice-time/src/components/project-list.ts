d181b7e68582defe258b597f548a20f23bd78670
Jay T <jaytula@gmail.com>
2020-07-17 14:42:26 -0700

    Modules: Understanding various Import & Export Syntaxes

--- practice-time/src/components/project-list.ts
+++ practice-time/src/components/project-list.ts
@@ -3,7 +3,7 @@ import { Project, ProjectStatus } from "../models/project.js";
 import { ProjectItem } from "./project-item.js";
 import { autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
-import { Component } from "./base-component.js";
+import Component from "./base-component.js";
 
 export class ProjectList extends Component<HTMLDivElement, HTMLElement>
   implements DragTarget {
