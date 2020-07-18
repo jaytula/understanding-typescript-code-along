df0046dc174fab4216ea14e6aa3a7e235224530c
Jay T <jaytula@gmail.com>
2020-07-17 21:27:09 -0700

    Webpack: Adding TypeScript Support with the ts-loader Package

--- practice-time/webpack.config.js
+++ practice-time/webpack.config.js
@@ -1,9 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/app.ts',
+  entry: "./src/app.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
-  }
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/ }],
+  },
 };
