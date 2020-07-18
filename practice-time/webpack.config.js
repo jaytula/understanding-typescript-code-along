98f951e40f558a2febfe9dd9494cf957e54abc55
Jay T <jaytula@gmail.com>
2020-07-18 16:16:07 -0700

    Webpack: Finishing the Setup & Adding webpack-dev-server

--- practice-time/webpack.config.js
+++ practice-time/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
 module.exports = {
+  mode: 'development',
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "dist"),
+    publicPath: 'dist'
   },
   devtool: 'inline-source-map',
   resolve: {
@@ -13,4 +15,7 @@ module.exports = {
   module: {
     rules: [{ test: /\.ts$/, loader: "ts-loader", exclude: /node_modules/ }],
   },
+  devServer: {
+    port: 9090
+  }
 };
