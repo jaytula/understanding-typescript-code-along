d2b27344d703ca705145e5e96084208e43d3f7a6
Jay T <jaytula@gmail.com>
2020-07-20 11:49:54 -0700

    Pretty format

--- third-party-libs/src/app.ts
+++ third-party-libs/src/app.ts
@@ -1,13 +1,11 @@
-import 'reflect-metadata';
-import {plainToClass} from 'class-transformer';
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 import { Product } from "./product.model";
-
 
 const products = [
   { title: "A Carpet", price: 29.99 },
   { title: "A Book", price: 10.99 },
 ];
-
 
 // const p1 = new Product("A Book", 12.99);
 // console.log(p1.getInformation());
