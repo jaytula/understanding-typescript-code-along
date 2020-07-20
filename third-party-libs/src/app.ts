e812f4afe8c7914b8c24e9682ac130dbb19e9264
Jay T <jaytula@gmail.com>
2020-07-20 10:41:01 -0700

    3rd Party Libs: No Types Needed: class-transformer

--- third-party-libs/src/app.ts
+++ third-party-libs/src/app.ts
@@ -1,7 +1,22 @@
-import _ from "lodash";
+import 'reflect-metadata';
+import {plainToClass} from 'class-transformer';
+import { Product } from "./product.model";
 
-declare var GLOBAL: any;
 
-console.log(_.shuffle([1, 2, 3, 4, 5]));
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
 
-console.log(GLOBAL);
+
+// const p1 = new Product("A Book", 12.99);
+// console.log(p1.getInformation());
+
+// const loadedProducts = products.map(
+//   prod => new Product(prod.title, prod.price)
+// );
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
