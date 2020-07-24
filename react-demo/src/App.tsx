94e1b39c9bb4a154a5a4d5d401e3f40fcb4e3eaf
Jay T <jaytula@gmail.com>
2020-07-23 23:59:33 -0700

    React: Cross-Component Communication

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
 
+const initialTodos = [{id: 't1', text: 'Finish the course'}]
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'Finish the course'}];
+  const [todos, setTodos] = useState(initialTodos);
 
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+    setTodos(todos => [...todos, {id: new Date().toString(), text: text}]);
+  }
   return <div className="App">
-    {/* A component that adds todos */}
-    <NewTodo />
+    <NewTodo onAddTodo={todoAddHandler} />
     <TodoList items={todos} />
   </div>;
 };
