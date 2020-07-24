09d9691fb7b1a672ed27e3615622b97510c593b4
Jay T <jaytula@gmail.com>
2020-07-24 15:52:14 -0700

    React: More Props & State Work

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -8,12 +8,20 @@ const App: React.FC = () => {
 
   const todoAddHandler = (text: string) => {
     console.log(text);
-    setTodos(prevTodos => [...prevTodos, { id: Math.random().toString(), text: text }]);
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+  };
+
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
