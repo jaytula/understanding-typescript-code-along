566f190cc55d78eff96e86abf613e7e569d1b130
Jay T <jaytula@gmail.com>
2020-07-27 09:36:33 -0700

    Node: More CRUD Operations

--- node-types-demo/src/controllers/todos.ts
+++ node-types-demo/src/controllers/todos.ts
@@ -11,3 +11,32 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.status(201).json({ message: "Created the todo.", createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const { id: todoId } = req.params;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex === -1) {
+    throw new Error("Could not find todo");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText)
+
+  res.json({ message: "Updated", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const { id } = req.params;
+
+  const deleteIndex = TODOS.findIndex(e => e.id === id);
+  TODOS.splice(deleteIndex, 1);
+
+  res.json({ message: "Todo deleted" });
+};
